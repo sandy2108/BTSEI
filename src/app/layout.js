df03bd4e2eabab0e0068e9ffa851c9f672e70234
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-01-03 21:13:30 +0530

    meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,26 +3,17 @@ import './globals.css'
 export const metadata = {
   title: '$BTSEI ',
   description: 'No Team, No Roadmap,Just Satoshis Legacy',
-  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fcai-32252.appspot.com/o/Btc.png?alt=media&token=2fc0fbfb-8b8d-4b25-85c9-d776225015c6',
+  keywords: ["btsei", "sei", "Satoshis", "Sei meme"],
+  openGraph: {
+    images: 'https://firebasestorage.googleapis.com/v0/b/gpuxdrops.appspot.com/o/banner.png?alt=media&token=dfb3c5d4-3817-45aa-a026-d7eaf14e6b23'
+  }
+
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
 
-        {/* Open Graph metadata */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.imageUrl} /> {/* Add OG image tag */}
-
-        {/* Twitter Card metadata */}
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.imageUrl} /> {/* Add Twitter image tag */}
-      </Head>
       <body>{children}</body>
     </html>
   )
