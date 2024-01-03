bfe40361b045add368631cf84653d0bd4965f9fb
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-01-03 20:40:41 +0530

    changed the nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
                         <Image src="/Logo.png" alt="logo" width={60} height={60} />
 
                     </div>
-                    <div className='flex items-center justify-between md:gap-x-10 gap-x-2 text-[#FF9DA6]'>
-                        <Link href="/" className='text-[14px] leading-[21px]'>CONTACT US</Link>
-                        <Link href="/" className='text-[14px] leading-[21px]'>ABOUT US</Link>
+                    <div className='flex items-center justify-between md:gap-x-10 gap-x-2 text-[#FF9DA6] z-20'>
+                        <Link target='_blank' href="https://coinhall.org/sei/sei1slanyyjzw06j0dr49tzkfnzjmx6wckhcpm75dx7azh50y0m2t5sq37vuzg" className='text-[14px] leading-[21px]'>CHART</Link>
+                        <Link target='_blank' href="https://sei.astroport.fi/swap?to=usei&from=factory/sei1qyfgukz2q333fdx3sr07560set7r0v0qe2326a/BTSEI" className='text-[14px] leading-[21px]'>BUY </Link>
                     </div>
 
                 </div>
