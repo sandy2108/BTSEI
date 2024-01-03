e4f4a7f00d6f5a924a000770a5acc5a1e38dc913
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-01-03 21:33:04 +0530

    meta updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,5 @@
 import Head from 'next/head';
 import './globals.css'
-export const metadata = {
-  title: '$BTSEI',
-  description: 'No Team, No Roadmap, Just Satoshis Legacy',
-  keywords: ['btsei', 'sei', 'Satoshis', 'Sei meme'],
-  openGraph: {
-    title: '$BTSEI',
-    description: 'No Team, No Roadmap, Just Satoshis Legacy',
-    image: 'https://firebasestorage.googleapis.com/v0/b/wizard-4fa3a.appspot.com/o/banner.png?alt=media&token=917e087f-4600-4fa0-8d6e-327072556156',
-    card: 'summary_large_image',
-    twitter: {
-      card: 'summary_large_image',
-      site: '@btsei_token', // Replace with your Twitter handle
-      title: '$BTSEI',
-      description: 'No Team, No Roadmap, Just Satoshis Legacy',
-      image: 'https://firebasestorage.googleapis.com/v0/b/wizard-4fa3a.appspot.com/o/banner.png?alt=media&token=917e087f-4600-4fa0-8d6e-327072556156',
-    },
-  }
-};
 
 
 export default function RootLayout({ children }) {
--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,18 @@ export default function Home() {
   return (
     <main className="bg-black min-h-screen "
     >
+      <Head>
+        <Head>
+          <title>$BTSEI</title>
+          <meta name="description" content="No Team, No Roadmap, Just Satoshis Legacy" />
+          <meta property="og:title" content="$BTSEI" />
+          <meta property="og:description" content="No Team, No Roadmap, Just Satoshis Legacy" />
+          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/wizard-4fa3a.appspot.com/o/banner.png?alt=media&token=917e087f-4600-4fa0-8d6e-327072556156" />
+          <meta name="twitter:card" content="summary_large_image" />
+
+        </Head>
+
+      </Head>
       <Navbar />
       <Hero />
       <ToastContainer />
