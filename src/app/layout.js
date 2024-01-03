c9efa7293dead9c8b08c8e61981b5e875b28ea9e
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-01-03 21:18:34 +0530

    meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,18 @@
 import Head from 'next/head';
 import './globals.css'
 export const metadata = {
-  title: '$BTSEI ',
-  description: 'No Team, No Roadmap,Just Satoshis Legacy',
-  keywords: ["btsei", "sei", "Satoshis", "Sei meme"],
+  title: '$BTSEI',
+  description: 'No Team, No Roadmap, Just Satoshis Legacy',
+  keywords: ['btsei', 'sei', 'Satoshis', 'Sei meme'],
   openGraph: {
-    images: 'https://firebasestorage.googleapis.com/v0/b/gpuxdrops.appspot.com/o/banner.png?alt=media&token=dfb3c5d4-3817-45aa-a026-d7eaf14e6b23'
+    title: '$BTSEI',
+    description: 'No Team, No Roadmap, Just Satoshis Legacy',
+    image: 'https://firebasestorage.googleapis.com/v0/b/gpuxdrops.appspot.com/o/banner.png?alt=media&token=dfb3c5d4-3817-45aa-a026-d7eaf14e6b23',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gpuxdrops.appspot.com/o/banner.png?alt=media&token=dfb3c5d4-3817-45aa-a026-d7eaf14e6b23',
+    card: 'summary_large_image',
   }
-
 };
+
 
 export default function RootLayout({ children }) {
   return (
