61820e913c7e00f8c82801e8daf308284d4f2c2e
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-01-03 21:52:27 +0530

    final meta

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import { ToastContainer } from 'react-toastify';
 import Head from 'next/head';
+
+export const metadata = {
+  title: '$BTSEI',
+  description: '“No team . No roadmap. Just Seitoshi legacy”',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/onepercent-63fd5.appspot.com/o/Logo.jpg?alt=media&token=2a7b87bb-2ed9-43a1-91cb-d99fba3dc994',
+};
 
 
 export default function Home() {
@@ -9,17 +16,20 @@ export default function Home() {
     <main className="bg-black min-h-screen "
     >
       <Head>
-        <Head>
-          <title>$BTSEI</title>
-          <meta name="description" content="No Team, No Roadmap, Just Satoshis Legacy" />
-          <meta property="og:title" content="$BTSEI" />
-          <meta property="og:description" content="No Team, No Roadmap, Just Satoshis Legacy" />
-          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/wizard-4fa3a.appspot.com/o/Logo.png?alt=media&token=724bdd4b-91af-45f8-abc9-4416f65c97e7" />
-          <meta name="twitter:card" content="summary_large_image" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
 
-        </Head>
+        {/* Open Graph metadata */}
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.imageUrl} /> {/* Add OG image tag */}
 
+        {/* Twitter Card metadata */}
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.imageUrl} /> {/* Add Twitter image tag */}
       </Head>
+
       <Navbar />
       <Hero />
       <ToastContainer />
