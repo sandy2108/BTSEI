3157f9f538bc380864cf37655b8780b98dc8b220
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-01-03 15:48:36 +0530

    navbar font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                         <Image src="/Logo.png" alt="logo" width={60} height={60} />
 
                     </div>
-                    <div className='flex items-center justify-between gap-x-4 text-[#FF9DA6]'>
+                    <div className='flex items-center justify-between md:gap-x-10 gap-x-2 text-[#FF9DA6]'>
                         <Link href="/" className='text-[14px] leading-[21px]'>CONTACT US</Link>
                         <Link href="/" className='text-[14px] leading-[21px]'>ABOUT US</Link>
                     </div>
